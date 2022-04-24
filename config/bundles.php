730ae4274ab9d9ba1e74e28709631fd920ceed5a
mprizmic <mprizmic@gmail.com>
2022-04-23 22:50:08 -0300

    se agrega un bundle knp de paginacion. se agrega el bundle fos para rest.

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,6 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
