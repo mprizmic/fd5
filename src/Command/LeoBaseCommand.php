328ae97ac0f3a85a3c54600ca4a123048daeb539
mprizmic <mprizmic@gmail.com>
2022-05-16 18:32:39 -0300

    comando de prueba

---
+++ src/Command/LeoBaseCommand.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * lee los establecimientos del repositorio y muestra los apodos por pantalla
+ * tambien los guarda en un log
+ * 
+ */
+
+namespace App\Command;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Psr\Log\LoggerInterface;
+
+class LeoBaseCommand extends Command {
+
+    protected static $defaultName = 'app:leo-base';
+    protected static $defaultDescription = 'prueba de acceso a datos. Tomado de MigrarLocalización de fd5.local';
+    private $em;
+    private $logger;
+
+    public function __construct(EntityManagerInterface $em, LoggerInterface $logger) {
+        parent::__construct();
+        $this->em = $em;
+        $this->logger = $logger;
+    }
+
+    protected function configure(): void {
+        $this
+                ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
+                ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int {
+
+        $estas = $this->em->getRepository('App\Entity\Establecimiento')->findAllOrdenado();
+
+        foreach ($estas as $esta) {
+            $mensaje = 'Establecimiento: ' . $esta->getApodo();
+            $output->writeln('**********************************************');
+            $output->writeln($mensaje);
+            $this->logger->info($mensaje);
+        }
+//        //salida de pantalla
+        $output->writeln('//////////////////////////////////////////////');
+        $output->writeln('//////////////////////////////////////////////');
+        $output->writeln('Proceso todo');
+
+        return Command::SUCCESS;
+    }
+
+}
--- src/Controller/PruebaController.php
+++ src/Controller/PruebaController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Model;
+namespace App\Controller;
 
 use App\Model\ConstantesGenerales;
 use App\Model\ConstantesSNSD;
